b4579deecc018b50ff05a89004168e78244b6a77
ying-sheng <sunnysheng55@gmail.com>
2014-04-09 10:38:42 +0200

    add more annotations from HGMD in addHGMD.pl

--- script/variantCalling/steps/addHGMD.pl
+++ script/variantCalling/steps/addHGMD.pl
@@ -26,7 +26,7 @@ foreach my $eachAnn (@annotationFiles){
 
   my $resourceTag = join '_', "HGMD", $tag;
 
-  $tagExpression = "-E $resourceTag.amino -E $resourceTag.acc_num -E $resourceTag.disease -E $resourceTag.amimid -E $resourceTag.tag -E $resourceTag.comments -E $resourceTag.pmid -E $resourceTag.location -E $resourceTag.locref -E $resourceTag.deletion -E $resourceTag.insertion -E $resourceTag.type -E $resourceTag.score -E $resourceTag.wildtype ";
+  $tagExpression = "-E $resourceTag.amino -E $resourceTag.acc_num -E $resourceTag.disease -E $resourceTag.omimid -E $resourceTag.tag -E $resourceTag.comments -E $resourceTag.pmid -E $resourceTag.location -E $resourceTag.locref -E $resourceTag.deletion -E $resourceTag.insertion -E $resourceTag.type -E $resourceTag.score -E $resourceTag.wildtype ";
 
   $resultVCF = join '.', "all.filter", $tag, "vcf";
 
@@ -35,7 +35,7 @@ foreach my $eachAnn (@annotationFiles){
 
   print "java -Xmx4g -jar /Volumes/data.odin/ying/researchBundle/tools/GenomeAnalysisTK-2.8-1-g932cd3a/GenomeAnalysisTK.jar \n-T VariantAnnotator \n-R /Volumes/data.odin/ying/researchBundle/refData/dataDistro_r01_d01/b37/genomic/gatkBundle_2.5/human_g1k_v37_decoy.fasta \n--variant $inputVCF \n-L $inputVCF \n--resource:$resourceTag $eachAnn \n$tagExpression \n-o $resultVCF\n2>$err >$info\n\n";
 
-  system "java -Xmx4g -jar /Volumes/data.odin/ying/researchBundle/tools/GenomeAnalysisTK-2.8-1-g932cd3a/GenomeAnalysisTK.jar \n-T VariantAnnotator \n-R /Volumes/data.odin/ying/researchBundle/refData/dataDistro_r01_d01/b37/genomic/gatkBundle_2.5/human_g1k_v37_decoy.fasta --variant $inputVCF -L $inputVCF --resource:$resourceTag $eachAnn $tagExpression -o $resultVCF 2>$err >$info";
+  system "java -Xmx4g -jar /Volumes/data.odin/ying/researchBundle/tools/GenomeAnalysisTK-2.8-1-g932cd3a/GenomeAnalysisTK.jar -T VariantAnnotator -R /Volumes/data.odin/ying/researchBundle/refData/dataDistro_r01_d01/b37/genomic/gatkBundle_2.5/human_g1k_v37_decoy.fasta --variant $inputVCF -L $inputVCF --resource:$resourceTag $eachAnn $tagExpression -o $resultVCF 2>$err >$info";
 
   system "intersectBed -wa -a $eachAnn -b $inputVCF >> hgmd.vcf";
 
